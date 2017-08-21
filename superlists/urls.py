28757c23ece97a0ffe16af0e6c8f4ffa0be26b8d
Dustin O'Brien <dcobrien82@gmail.com>
2017-08-21 15:49:25 -0700

    Basic view now returns mininam HTML

--- superlists/urls.py
+++ superlists/urls.py
@@ -13,11 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
-urlpatterns = [
-    url(r'^$', 'lists.views.home_page', name='home')
-
-    # url(r'^admin/', admin.site.urls),
-]
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'superlists.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
+    # url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', 'lists.views.home_page', name='home'),
+)
